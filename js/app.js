880b6538cb7277ddc891c1a72077d0e6b21f1370
619sd <158139968+619sd@users.noreply.github.com>
2025-06-03 23:59:14 -0700

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,41 @@
+const clientId = '4340319ae0c4474292cdcd5a01fa141e';
+const redirectUri = 'http://10.76.20.240:80/callback';
+const scopes = [
+  'user-read-playback-state',
+  'user-modify-playback-state',
+  'user-read-currently-playing'
+];
+
+function generateCodeVerifier(length = 128) {
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~';
+  let verifier = '';
+  for (let i = 0; i < length; i++) {
+    verifier += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return verifier;
+}
+
+async function generateCodeChallenge(verifier) {
+  const encoder = new TextEncoder();
+  const data = encoder.encode(verifier);
+  const digest = await window.crypto.subtle.digest('SHA-256', data);
+  return btoa(String.fromCharCode(...new Uint8Array(digest)))
+    .replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
+}
+
+document.getElementById('loginBtn').addEventListener('click', async () => {
+  const codeVerifier = generateCodeVerifier();
+  const codeChallenge = await generateCodeChallenge(codeVerifier);
+  localStorage.setItem('code_verifier', codeVerifier);
+
+  const args = new URLSearchParams({
+    response_type: 'code',
+    client_id: clientId,
+    scope: scopes.join(' '),
+    redirect_uri: redirectUri,
+    code_challenge_method: 'S256',
+    code_challenge: codeChallenge
+  });
+
+  window.location = `https://accounts.spotify.com/authorize?${args.toString()}`;
+});
